fc1909c56bfbcac2d5501bb71a368fb98684b283
Tobias Racedo <tobaracedo@gmail.com>
2022-06-13 23:16:56 -0300

    index back to /

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = require('express').Router();
 //display an specific game
 router.get('/:id', showGame)
 //display  games
-router.get('/index', showGames)
+router.get('/', showGames)
 //create a new game
 router.post('/', createGame)
 //update a game
